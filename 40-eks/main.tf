ef68ec85742d17553c49e1dcdadd4e3e9bc586c9
akhildanda <akhildanda7@gmail.com>
2024-07-18 21:02:16 +0530

    eks

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -13,7 +13,7 @@ module "eks" {
   cluster_name    = "${var.project_name}-${var.environment}"
   cluster_version = "1.30"
   # it should be false in PROD environments
-  cluster_endpoint_public_access = false
+  cluster_endpoint_public_access = true
 
   vpc_id                   = local.vpc_id
   subnet_ids               = split(",", local.private_subnet_ids)
